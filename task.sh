3a724f29964e274a7dad19aa79f185fbb693bbf8
David Bingham <git@davidjbingham.co.uk>
2016-08-23 15:47:28 +0000

    Removed `execute` task (replaced with what was `exec`), renamed some task arguments and made `help` task a little more robust.

--- task.sh
+++ task.sh
@@ -16,12 +16,12 @@
 ##
 
 # Task to execute from this script
-task='help'
+task=''
 
 # Fleet-wide arguments
 tag='djbingham/fleet-certificate-authority'
 container='fleet-certificate-authority'
-certificatesVolume='fleet-certificates'
+certificateVolume='fleet-certificates'
 
 # Host-specific arguments
 host=''
@@ -29,8 +29,8 @@ privateIP=''
 publicIP=''
 
 # Command to execute in container
-entrypoint=''
-command=''
+command='/app/scripts/main.sh'
+arguments=''
 
 
 while [[ $# -gt 0 ]]
@@ -48,8 +48,8 @@ do
 			shift # past argument name
 			;;
 
-		--certificatesVolume)
-			certificatesVolume="$2"
+		--certificateVolume)
+			certificateVolume="$2"
 			shift # past argument name
 			;;
 
@@ -68,21 +68,21 @@ do
 			shift # past argument name
 			;;
 
-		--entrypoint)
-			entrypoint="$2"
-			shift # past argument name
-			;;
-
-		--cmd,--command)
+		--command,cmd)
 			command="$2"
 			shift # past argument name
 			;;
 
+		--arguments)
+			arguments="$2"
+			shift # past argument name
+			;;
+
 		*)
-			if [[ ${task} == 'help' ]]; then
+			if [[ ${task} == '' ]]; then
 				task="${key}"
 			else
-				echo "Unrecognised option given: ${key}."
+				arguments="${arguments} ${key}"
 			fi
 			;;
 	esac
@@ -137,7 +137,7 @@ case ${task} in
 		docker run \
 			-d \
 			--name "${container}" \
-			--volume "${certificatesVolume}:/app/certificates" \
+			--volume "${certificateVolume}:/app/certificates" \
 			${tag}
 		;;
 
@@ -153,7 +153,7 @@ case ${task} in
 		docker run \
 			-d \
 			--name "${container}" \
-			--volume "${certificatesVolume}:/app/certificates" \
+			--volume "${certificateVolume}:/app/certificates" \
 			--volume "$(pwd)/scripts:/app/scripts" \
 			--volume "$(pwd)/config:/app/config" \
 			${tag}
@@ -172,7 +172,7 @@ case ${task} in
 	add-certificate)
 		docker run \
 			--rm \
-			--volume "${certificatesVolume}:/app/certificates" \
+			--volume "${certificateVolume}:/app/certificates" \
 			${tag} add-certificate ${host} ${privateIP} ${publicIP}
 		;;
 
@@ -187,38 +187,16 @@ case ${task} in
 
 	##
 	##> execute
-	##>> Execute a command within a container sharing volumes and image with the running Fleet CA container.
+	##>> Execute a command within the running Fleet CA container. The `help` command outputs documentation.
 	##
 	##>> Allowed arguments:
 	##>>> certificateVolume: Name of the volume in which generated certificates should be stored.
 	##>>> tag: The tag of the container image to run.
-	##>>> entrypoint: The entrypoint instruction to pass to the docker run command.
-	##>>> command (or cmd): The cmd instruction to pass to the docker run command.
+	##>>> command (or cmd): The command to execute inside the container
 	execute)
-		docker run \
-			--rm \
+		docker exec \
 			-it \
-			--volume "${certificatesVolume}:/app/certificates" \
-			--entrypoint ${entrypoint} \
-			${tag} ${command}
-		;;
-
-	##
-	##> exec
-	##>> Execute a command within the running Fleet CA container.
-	##
-	##>> Allowed arguments:
-	##>>> certificateVolume: Name of the volume in which generated certificates should be stored.
-	##>>> tag: The tag of the container image to run.
-	##>>> entrypoint: The entrypoint instruction to pass to the docker run command.
-	##>>> command (or cmd): The cmd instruction to pass to the docker run command.
-	exec)
-		docker run \
-			--rm \
-			-it \
-			--volume "${certificatesVolume}:/app/certificates" \
-			--entrypoint ${entrypoint} \
-			${tag} ${command}
+			${container} ${command} ${arguments}
 		;;
 
 	##
@@ -231,19 +209,19 @@ case ${task} in
 	destroy)
 		docker stop ${container} || true
 		docker rm -vf ${container} || true
-		docker volume rm ${certificatesVolume} || true
+		docker volume rm ${certificateVolume} || true
 		;;
 
 	##
 	##> help
 	##>> Print this help documentation.
 	help)
-		file=`basename "$0"`
+		file="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/`basename ${BASH_SOURCE[0]}`"
 		docs=''
-		grep '^[[:space:]]*##' "$0" | while read -r line; do
+		grep '^[[:space:]]*##' "$file" | while read -r line; do
 			indentLevel=$(echo ${line} | sed 's/##\(>*\)/\1/g' | awk '{ print length }')
 			indent=$(printf '\t'{1..${indentLevel})
-			echo ${line} | sed -e 's/^[[:space:]]*##//' -e 's/>/    /g' -e "s/_PROG_/$file/" 1>&2
+			echo ${line} | sed -e 's/^[[:space:]]*##//' -e 's/>/    /g' -e "s|_PROG_|$file|" 1>&2
 		done
 		;;
 
