23db5d719cdf8045fa9ca85eec1041e86a6ef1d4
David Bingham <git@davidjbingham.co.uk>
2016-08-23 15:45:49 +0000

    Added documentation to main script.

--- scripts/main.sh
+++ scripts/main.sh
@@ -10,19 +10,45 @@ fi
 
 echo "Running command '${command}'"
 case ${command} in
+	##
+	##> auto
+	##>> Continually monitor and automatically renew all certificates generated using `add-certificate`.
+	##>> This includes certificates added after the `auto` command was started.
 	auto)
 		initialiseCA
 		automate 10 renewAllCertificates
 		;;
 
+	##
+	##> add-certificate
+	##>> Generate and deploy the initial certificate for a new host.
+	##>> This will be automatically renewed before expiry if there is a running `auto` command.
+	##
+	##>> Expected arguments:
+	##>>> hostName
+	##>>> publicIP
+	##>>> privateIP
 	add-certificate)
 		createCSR $@
 		generateCertificate $@
 		deployCertificate $@
 		;;
 
+	##
+	##> help
+	##>> Print this help documentation.
+	help)
+		file="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/`basename ${BASH_SOURCE[0]}`"
+		echo "FILE: ${file}"
+		docs=''
+		grep '^[[:space:]]*##' "$file" | while read -r line; do
+			indentLevel=$(echo ${line} | sed 's/##\(>*\)/\1/g' | awk '{ print length }')
+			indent=$(printf '\t'{1..${indentLevel})
+			echo ${line} | sed -e 's/^[[:space:]]*##//' -e 's/>/    /g' -e "s|_PROG_|$file|" 1>&2
+		done
+		;;
+
 	*)
-		declare -a allowedCommands=(auto add-certificate)
-		echo "Command not recognised. Allowed commands are: ${allowedCommands[@]}"
+		echo "Command not recognised. Use the `help` command to see what commands are available."
 		;;
 esac
